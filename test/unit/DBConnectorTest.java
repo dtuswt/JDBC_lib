78fb898ee23df72326a2e57603f4926a6884cc5c
awo-dev <!9HdGbpfM?>
2017-06-05 14:56:55 +0200

    travis sql user

--- test/unit/DBConnectorTest.java
+++ test/unit/DBConnectorTest.java
@@ -13,7 +13,7 @@ import static org.junit.Assert.*;
 public class DBConnectorTest {
     @Test
     public void TestConnectDatabase() throws Exception {
-        IConnector db = new DBConnector();
+        IConnector db = new DBConnector("127.0.0.1", 3306, "dbweight", "travis", "");
 
         try {
             db.connectToDatabase();
