9e0f7f3957393ba3c146cb5bdb8b0d019e99c5f6
awo-dev <!9HdGbpfM?>
2017-06-09 18:41:56 +0200

    Updated project to be a maven project

---
+++ src/main/java/jdbclib/DatabaseConnection.java
@@ -0,0 +1,50 @@
+package jdbclib;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * Created by awo on 06/06/17.
+ */
+public class DatabaseConnection {
+    private String host;
+    private int port;
+    private String database;
+    private String username;
+    private String password;
+
+    public DatabaseConnection(String host, int port, String database, String username, String password) {
+        this.host = host;
+        this.port = port;
+        this.database = database;
+        this.username = username;
+        this.password = password;
+    }
+
+    public DatabaseConnection() throws IOException, ArithmeticException {
+        File file = new File(".env");
+        Properties props = null;
+
+        try (FileInputStream fileInputStream = new FileInputStream(file)){
+            props = new Properties();
+            props.load(fileInputStream);
+        } catch (IOException e) {
+            throw new IOException("File not found.");
+        }
+
+        database = props.getProperty("DB_DATABASE");
+        username = props.getProperty("DB_USERNAME");
+        password = props.getProperty("DB_PASSWORD");
+        host     = props.getProperty("DB_HOST"); try {
+            port     = Integer.parseInt(props.getProperty("DB_PORT"));
+        } catch (ArithmeticException e){ throw new ArithmeticException("DB_PORT not an integer."); }
+    }
+
+    public String getHost()     { return host;     }
+    public int    getPort()     { return port;     }
+    public String getDatabase() { return database; }
+    public String getUsername() { return username; }
+    public String getPassword() { return password; }
+}
